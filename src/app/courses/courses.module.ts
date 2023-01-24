96baf85e77ec4cb07d3e9c762302c17e4d239bfa
Guilherme Santos Araújo <guilherme1984.araujo@gmail.com>
2023-01-23 22:18:46 -0300

    melhorando o css e criando uma pasta para organizar os imports

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
+  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule],
 })
 export class CoursesModule {}
---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [MatCardModule, MatTableModule, MatToolbarModule],
+})
+export class AppMaterialModule {}
+
+//aula 7 9' a 10'02"
